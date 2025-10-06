ad8ae6acdea6b8da3111c90730c379afa207ef78
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-06 16:13:16 +0530

    commit

--- app.js
+++ app.js
@@ -34,6 +34,20 @@ boxes.forEach((box) => {
 
 const checkWinner = () => {
     for(let pattern of winPatterns){
-        console.log(pattern[0],pattern[1],pattern[2]);
+        console.log(
+            boxes[pattern[0]].innerText,
+            boxes[pattern[1]].innerText,
+            boxes[pattern[2]].innerText
+        );
+
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val !="" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2val && pos2Val === pos3Val){
+                    console.log("winner", pos1Val);
+            }
+        }
     }
-}
+};
