48a55cc0b51b3d7f1de365aa233d0443f0e3b677
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-06 15:26:23 +0530

    commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let turnO = true; //playerX, playerO
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if(trunO){ //palyerO
+            box.innerText = "0";
+            turnO = false;
+        }
+        else{//PalyerX
+            box.innerText = "X";
+            turnO = true;
+        }
+    });
+});
